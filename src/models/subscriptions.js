7544a23f812cf1db00d715b25951cce010b28b66
11midooadel <11midooadel@gmail.com>
2026-08-28 20:38:21 +0300

    Implement health record management with CRUD operations and schema definition

--- src/models/subscriptions.js
+++ src/models/subscriptions.js
@@ -13,11 +13,7 @@ const SubscriptionSchema = new mongoose.Schema({
 	},
 	paymentId: {
 		type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
-		ref: 'Payment' // Links to the payment receipt/transaction details
-=======
 		ref: 'Payment' // References the Payment model to the payment receipt/transaction details
->>>>>>> origin/main
 	},
 	startDate: {
 		type: Date,
