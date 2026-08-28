0f941949f782478e440da3265627b84ea7b2dcaf
11midooadel <11midooadel@gmail.com>
2026-08-28 20:52:58 +0300

    Revert index.js and set up routes for health records

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/gms")
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+const port = 3000;
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.json());
+app.use("/users", require("./routes/users"));
+app.use("/payments", require("./routes/payments"));
+app.use("/classes", require("./routes/classes"));
+app.use("/enrollments", require("./routes/ClassEnrollment"));
+app.use("/attendance", require("./routes/attendance"));
+app.use("/plans", require("./routes/plans"));
+app.use("/subscriptions", require("./routes/subscriptions"));
+app.use("/members", require("./routes/members"));
+app.use("/trainers", require("./routes/trainers"));
+app.use("/healthRecord", require("./routes/healthRecord"));
